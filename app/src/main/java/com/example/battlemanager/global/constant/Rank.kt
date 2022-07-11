5105f768caa9b8bdb17ef9f17b5e4a581d52cc23
여름 <pjw7200@ajou.ac.kr>
2022-07-11 21:58:33 +0900

    랭크 추가

---
+++ app/src/main/java/com/example/battlemanager/global/constant/Rank.kt
@@ -0,0 +1,14 @@
+package com.example.battlemanager.global.constant
+
+class Rank {
+    companion object {
+        const val ATTACK = 0
+        const val DEFENSE = 1
+        const val SPECIAL_ATTACK = 2
+        const val SPECIAL_DEFENSE = 3
+        const val SPEED = 4
+        const val ACCURACY = 5
+        const val EVASION_RATE = 6
+        const val CRITICAL_RATE = 7
+    }
+}
