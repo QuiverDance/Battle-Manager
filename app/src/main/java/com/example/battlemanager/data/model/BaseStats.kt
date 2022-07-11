9c5b605efce55f9a606a74be4d9160f0c0ecb7ea
여름 <pjw7200@ajou.ac.kr>
2022-07-11 21:36:04 +0900

    종족값 추가

---
+++ app/src/main/java/com/example/battlemanager/data/model/BaseStats.kt
@@ -0,0 +1,10 @@
+package com.example.battlemanager.data.model
+
+data class BaseStats (
+    val H : Int,
+    val A : Int,
+    val B : Int,
+    val C : Int,
+    val D : Int,
+    val S : Int,
+)
