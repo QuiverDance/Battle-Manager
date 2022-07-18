2da4eb451ff164e1dcd6dec3240308689ae53642
여름 <pjw7200@ajou.ac.kr>
2022-07-18 16:46:57 +0900

    포켓몬 세부 설정 프래그먼트

---
+++ app/src/main/java/com/example/battlemanager/presentation/PokemonSettingFragment.kt
@@ -0,0 +1,9 @@
+package com.example.battlemanager.presentation
+
+import com.example.battlemanager.R
+import com.example.battlemanager.databinding.FragmentPokemonSettingBinding
+import com.example.battlemanager.global.base.BaseFragment
+
+class PokemonSettingFragment : BaseFragment<FragmentPokemonSettingBinding>() {
+    override val layoutResourceId = R.layout.fragment_pokemon_setting
+}
