a4531057774c8d2582b3f5b5eddd7e7e800ce7b0
여름 <pjw7200@ajou.ac.kr>
2022-07-19 15:56:36 +0900

    메인 프래그먼트로 수정

--- app/src/main/java/com/example/battlemanager/presentation/MainActivity.kt
+++ app/src/main/java/com/example/battlemanager/presentation/MainActivity.kt
@@ -1,16 +1,9 @@
 package com.example.battlemanager.presentation
 
-import android.os.Bundle
 import com.example.battlemanager.R
 import com.example.battlemanager.databinding.ActivityMainBinding
 import com.example.battlemanager.global.base.BaseActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
     override val layoutResourceId = R.layout.activity_main
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
 }
---
+++ app/src/main/java/com/example/battlemanager/presentation/MainFragment.kt
@@ -0,0 +1,39 @@
+package com.example.battlemanager.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.viewModels
+import com.example.battlemanager.R
+import com.example.battlemanager.databinding.ActivityMainBinding
+import com.example.battlemanager.databinding.FragmentMainBinding
+import com.example.battlemanager.global.base.BaseActivity
+import com.example.battlemanager.global.base.BaseFragment
+import com.google.android.material.tabs.TabLayout
+
+class MainFragment : BaseFragment<FragmentMainBinding>() {
+    override val layoutResourceId = R.layout.fragment_main
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun initState() {
+        super.initState()
+    }
+
+    override fun initDataBinding() {
+        super.initDataBinding()
+        setViewPager()
+    }
+
+    fun setViewPager(){
+        with(binding.pokemonViewPager) {
+            adapter = PokemonSettingViewPagerAdapter(this@MainFragment)
+            isUserInputEnabled = false
+            binding.tabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    val position = tab!!.position
+                    currentItem = position
+                }
+                override fun onTabUnselected(tab: TabLayout.Tab?) {}
+                override fun onTabReselected(tab: TabLayout.Tab?) {}
+            })
+        }
+    }
+}
