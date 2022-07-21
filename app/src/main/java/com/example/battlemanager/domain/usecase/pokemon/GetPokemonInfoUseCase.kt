32e70b2f1320293a3b638c7b2d17baa1937d188a
여름 <pjw7200@ajou.ac.kr>
2022-07-21 21:19:01 +0900

    더미 데이터 추가

--- app/src/main/java/com/example/battlemanager/domain/usecase/pokemon/GetPokemonInfoUseCase.kt
+++ app/src/main/java/com/example/battlemanager/domain/usecase/pokemon/GetPokemonInfoUseCase.kt
@@ -1,10 +1,24 @@
 package com.example.battlemanager.domain.usecase.pokemon
 
+import com.example.battlemanager.domain.model.BaseStats
+import com.example.battlemanager.domain.model.FilterItem
 import com.example.battlemanager.domain.model.PokemonInfo
 import com.example.battlemanager.domain.repository.PokemonRepository
 
 class GetPokemonInfoUseCase(private val repository: PokemonRepository) {
     suspend fun invoke(id : Int) : PokemonInfo{
-        return repository.getPokemonInfo(id)
+        val pokemons = listOf(
+            PokemonInfo(1, 1, "이상해씨", "", listOf("풀", "독"), listOf("심록"), 3, 5f, BaseStats(50, 50, 50, 50, 50, 50)),
+            PokemonInfo(2, 2, "이상해풀", "", listOf("풀", "독"), listOf("심록"), 3, 5f, BaseStats(50, 50, 50, 50, 50, 50)),
+            PokemonInfo(3, 3, "이상해꽃", "", listOf("풀", "독"), listOf("심록"), 3, 5f, BaseStats(50, 50, 50, 50, 50, 50)),
+            PokemonInfo(4, 4, "파이리", "", listOf("불"), listOf("맹화"), 3, 5f, BaseStats(50, 50, 50, 50, 50, 50)),
+            PokemonInfo(5, 5, "리자드", "", listOf("불"), listOf("맹화"), 3, 5f, BaseStats(50, 50, 50, 50, 50, 50)),
+            PokemonInfo(6, 6, "리자몽", "", listOf("불", "비행"), listOf("맹화"), 3, 5f, BaseStats(50, 50, 50, 50, 50, 50)),
+            PokemonInfo(7, 7, "꼬부기", "", listOf("물"), listOf("급류"), 3, 5f, BaseStats(50, 50, 50, 50, 50, 50)),
+            PokemonInfo(8, 8, "어니부기", "", listOf("물"), listOf("급류"), 3, 5f, BaseStats(50, 50, 50, 50, 50, 50)),
+            PokemonInfo(9, 9, "거북왕", "", listOf("물"), listOf("급류"), 3, 5f, BaseStats(50, 50, 50, 50, 50, 50)),
+        )
+        //return repository.getPokemonInfo(id)
+        return pokemons[id]
     }
 }
