6daa9a0241f01dc995b13209609e32612ee68371
여름 <pjw7200@ajou.ac.kr>
2022-08-10 14:41:08 +0900

    최대 hp 초기화

--- app/src/main/java/com/example/battlemanager/presentation/BattleResultFragment.kt
+++ app/src/main/java/com/example/battlemanager/presentation/BattleResultFragment.kt
@@ -3,7 +3,6 @@ package com.example.battlemanager.presentation
 import androidx.fragment.app.viewModels
 import com.example.battlemanager.R
 import com.example.battlemanager.databinding.FragmentBattleResultBinding
-import com.example.battlemanager.databinding.FragmentMainBinding
 import com.example.battlemanager.global.base.BaseFragment
 import com.example.battlemanager.global.util.StatUtil
 
@@ -14,9 +13,19 @@ class BattleResultFragment : BaseFragment<FragmentBattleResultBinding>() {
     override fun initDataBinding() {
         super.initDataBinding()
         binding.viewModel = viewModel
+        setMaxHp()
     }
     private fun setMaxHp(){
         val pokemon = viewModel.pokemon2.value!!
         val maxHp = StatUtil.getHp(pokemon.pokemonInfo.name, pokemon.level, pokemon.pokemonInfo.baseStats.H, pokemon.individualValues.H, pokemon.effortValues.H)
+
+        binding.move1MaxHpProgressbar.max = maxHp
+        binding.move1MinHpProgressbar.max = maxHp
+        binding.move2MaxHpProgressbar.max = maxHp
+        binding.move2MinHpProgressbar.max = maxHp
+        binding.move3MaxHpProgressbar.max = maxHp
+        binding.move3MinHpProgressbar.max = maxHp
+        binding.move4MaxHpProgressbar.max = maxHp
+        binding.move4MinHpProgressbar.max = maxHp
     }
 }
