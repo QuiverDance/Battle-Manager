c20a151bb8cdb678dae5716c598c7a9a417fecd6
여름 <pjw7200@ajou.ac.kr>
2022-08-12 20:45:51 +0900

    TypeList 추가

--- app/src/main/java/com/example/battlemanager/global/util/TypeUtil.kt
+++ app/src/main/java/com/example/battlemanager/global/util/TypeUtil.kt
@@ -3,6 +3,7 @@ package com.example.battlemanager.global.util
 import java.lang.Exception
 
 object TypeUtil {
+    private val typeList = listOf("노말", "불꽃", "물", "전기", "풀", "얼음", "격투", "독", "땅", "비행", "에스퍼", "벌레", "바위", "고스트", "드래곤", "악", "강철", "페어리")
     private val typeTable = listOf(
         //      노     불    물    전     풀    얼    격    독     땅    비     에     벌     바    고    드    악     강     페
         listOf<Float>(    1f,    1f,    1f,    1f,    1f,    1f,    1f,    1f,    1f,    1f,    1f,     1f,  0.5f,    0f,    1f,    1f,  0.5f,    1f), //노말
@@ -25,27 +26,14 @@ object TypeUtil {
         listOf<Float>(    1f,  0.5f,    1f,    1f,    1f,    1f,    2f,  0.5f,    1f,    1f,    1f,     1f,    1f,    1f,    2f,    2f,  0.5f,    1f), //페어리
     )
     fun typeToString(type : Int) : String{
-        return when(type){
-            0 -> "노말"
-            1 -> "불꽃"
-            2 -> "물"
-            3 -> "전기"
-            4 -> "풀"
-            5 -> "얼음"
-            6 -> "격투"
-            7 -> "독"
-            8 -> "땅"
-            9 -> "비행"
-            10 -> "에스퍼"
-            11 -> "벌레"
-            12 -> "바위"
-            13 -> "고스트"
-            14 -> "드래곤"
-            15 -> "악"
-            16 -> "강철"
-            17 -> "페어리"
-            else -> throw Exception("It is not exist")
+        return typeList[type]
+    }
+    fun typeToInt(type : String) : Int{
+        for((idx, item) in typeList.withIndex()){
+            if(item == type)
+                return idx
         }
+        return -1
     }
     fun getTypeCorrelation(type1 : Int, type2 : Int) : Float{
         return typeTable[type1][type2]
