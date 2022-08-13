31240bb24c82bd724bd400a2a39050412aae56d0
여름 <pjw7200@ajou.ac.kr>
2022-08-13 13:52:10 +0900

    성격 값 순서 변경

--- app/src/main/java/com/example/battlemanager/global/constant/Nature.kt
+++ app/src/main/java/com/example/battlemanager/global/constant/Nature.kt
@@ -2,31 +2,31 @@ package com.example.battlemanager.global.constant
 
 class Nature {
     companion object {
-        const val LONELY = 0
-        const val ADAMANT = 1
-        const val NAUGHTY = 2
-        const val BRAVE = 3
-        const val BOLD = 4
-        const val IMPISH = 5
-        const val LAX = 6
-        const val RELAXED = 7
-        const val MODEST = 8
-        const val MILD = 9
-        const val RASH = 10
-        const val QUIET = 11
-        const val CALM = 12
-        const val GENTLE = 13
-        const val CAREFUL = 14
-        const val SASSY = 15
-        const val TIMID = 16
-        const val HASTY = 17
-        const val JOLLY = 18
-        const val NAIVE = 19
-        const val BASHFUL = 20
-        const val HARDY = 21
-        const val DOCILE = 22
-        const val QUIRKY = 23
-        const val SERIOUS = 24
-        const val NULL = -1
+        const val NULL = 0
+        const val LONELY = 1
+        const val ADAMANT = 2
+        const val NAUGHTY = 3
+        const val BRAVE = 4
+        const val BOLD = 5
+        const val IMPISH = 6
+        const val LAX = 7
+        const val RELAXED = 8
+        const val MODEST = 9
+        const val MILD = 10
+        const val RASH = 11
+        const val QUIET = 12
+        const val CALM = 13
+        const val GENTLE = 14
+        const val CAREFUL = 15
+        const val SASSY = 16
+        const val TIMID = 17
+        const val HASTY = 18
+        const val JOLLY = 19
+        const val NAIVE = 20
+        const val BASHFUL = 21
+        const val HARDY = 22
+        const val DOCILE = 23
+        const val QUIRKY = 24
+        const val SERIOUS = 25
     }
 }
--- app/src/main/java/com/example/battlemanager/global/util/NatureUtil.kt
+++ app/src/main/java/com/example/battlemanager/global/util/NatureUtil.kt
@@ -8,6 +8,7 @@ object NatureUtil {
         "냉정한", "차분한", "얌전한", "신중한", "건방진", "겁쟁이같은", "성급한", "명랑한", "천진난만한", "수줍음을 타는", "노력하는", "온순한", "변덕스러운", "성실한")
     private val natureTable = listOf(
         //              공     방    특공    특방     스
+        listOf<Float>(    1f,    1f,    1f,    1f,    1f), //None
         listOf<Float>(  1.1f,  0.9f,    1f,    1f,    1f), //외로움을 타는
         listOf<Float>(  1.1f,    1f,  0.9f,    1f,    1f), //고집스런
         listOf<Float>(  1.1f,    1f,    1f,  0.9f,    1f), //개구쟁이같은
@@ -33,7 +34,6 @@ object NatureUtil {
         listOf<Float>(    1f,    1f,    1f,    1f,    1f), //온순한
         listOf<Float>(    1f,    1f,    1f,    1f,    1f), //변덕스러운
         listOf<Float>(    1f,    1f,    1f,    1f,    1f), //성실한
-        listOf<Float>(    1f,    1f,    1f,    1f,    1f), //None
     )
     fun natureToString(nature : Int) : String{
         return natureList[nature]
