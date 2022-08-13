12b507afe0c74c784cf4dd237f8286e70cdc470c
여름 <pjw7200@ajou.ac.kr>
2022-08-13 13:54:06 +0900

    최대HP 설정

--- app/src/main/java/com/example/battlemanager/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/battlemanager/presentation/MainViewModel.kt
@@ -3,26 +3,26 @@ package com.example.battlemanager.presentation
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.example.battlemanager.domain.model.FilterItem
 import com.example.battlemanager.domain.model.Pokemon
-import com.example.battlemanager.domain.model.PokemonInfo
-import com.example.battlemanager.domain.usecase.pokemon.GetPokemonInfoUseCase
-import com.example.battlemanager.domain.usecase.pokemon.GetPokemonNameListUseCase
 import com.example.battlemanager.global.util.SingleLiveEvent
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import com.example.battlemanager.global.util.StatUtil
 
 class MainViewModel : ViewModel() {
 
     private val _pokemon1 = MutableLiveData<Pokemon>()
     private val _pokemon2 = MutableLiveData<Pokemon>()
-    val pokemon1: LiveData<Pokemon> get() =_pokemon1
-    val pokemon2: LiveData<Pokemon> get() =_pokemon2
+    val pokemon1: LiveData<Pokemon> get() = _pokemon1
+    val pokemon2: LiveData<Pokemon> get() = _pokemon2
 
-    fun setPokemon(pokemon: Pokemon, target: Int){
-        if(target == 1) _pokemon1.postValue(pokemon)
+    fun setPokemon(pokemon: Pokemon, target: Int) {
+        pokemon.maxHp = StatUtil.getHp(
+            pokemon.pokemonInfo.name,
+            pokemon.level,
+            pokemon.pokemonInfo.baseStats.H,
+            pokemon.individualValues.H,
+            pokemon.effortValues.H
+        )
+        if (target == 1) _pokemon1.postValue(pokemon)
         else _pokemon2.postValue(pokemon)
     }
 
@@ -30,12 +30,14 @@ class MainViewModel : ViewModel() {
     fun startPokemon1() {
         startPokemon1.call()
     }
+
     val startPokemon2 = SingleLiveEvent<Any>()
     fun startPokemon2() {
         startPokemon2.call()
     }
+
     val startResult = SingleLiveEvent<Any>()
-    fun onStartResult(){
+    fun onStartResult() {
         startResult.call()
     }
 }
