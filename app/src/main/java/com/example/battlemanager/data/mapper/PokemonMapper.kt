a73c65c08039000b4c65904be54a3c45d0c6bb4a
여름 <pjw7200@ajou.ac.kr>
2022-08-23 22:18:28 +0900

    일부 attribute nullable로 수정 및 mapper class 수정

--- app/src/main/java/com/example/battlemanager/data/mapper/PokemonMapper.kt
+++ app/src/main/java/com/example/battlemanager/data/mapper/PokemonMapper.kt
@@ -1,24 +1,25 @@
 package com.example.battlemanager.data.mapper
 
+import android.util.Log
 import com.example.battlemanager.data.model.PokemonResponse
 import com.example.battlemanager.domain.model.BaseStats
 import com.example.battlemanager.domain.model.PokemonInfo
 
 object PokemonMapper {
-    fun mapperToPokemonInfo(pokemonResponse: PokemonResponse) : PokemonInfo{
+    fun mapperToPokemonInfo(pokemonResponse: PokemonResponse): PokemonInfo {
         val abilityList = mutableListOf(pokemonResponse.ability1)
-        if(pokemonResponse.ability2 != "null")
+        if (pokemonResponse.ability2 != null)
             abilityList.add(pokemonResponse.ability2)
-        if(pokemonResponse.ability3 != "null")
+        if (pokemonResponse.ability3 != null)
             abilityList.add(pokemonResponse.ability3)
 
         return PokemonInfo(
             id = pokemonResponse.id,
             dexId = pokemonResponse.dexId,
             name = pokemonResponse.name,
-            imageUrl = pokemonResponse.imgUrl,
+            imageUrl = pokemonResponse.imageUrl,
             type1 = pokemonResponse.type1,
-            type2 = pokemonResponse.type2,
+            type2 = pokemonResponse.type2 ?: "",
             validAbilityList = abilityList,
             validGender = pokemonResponse.genderType,
             weight = pokemonResponse.weight,
--- app/src/main/java/com/example/battlemanager/data/model/PokemonResponse.kt
+++ app/src/main/java/com/example/battlemanager/data/model/PokemonResponse.kt
@@ -3,17 +3,17 @@ package com.example.battlemanager.data.model
 import com.google.gson.annotations.SerializedName
 
 data class PokemonResponse(
-    @SerializedName("id") val id : Int,
+    @SerializedName("id") val id : Long,
     @SerializedName("dexId") val dexId : Int,
     @SerializedName("name")val name : String,
-    @SerializedName("imgUrl")val imgUrl : String,
+    @SerializedName("imageUrl")val imageUrl : String,
     @SerializedName("type1")val type1 : String,
-    @SerializedName("type2")val type2 : String,
+    @SerializedName("type2")val type2 : String?,
     @SerializedName("ability1")val ability1 : String,
-    @SerializedName("ability2")val ability2 : String,
-    @SerializedName("ability3")val ability3 : String,
+    @SerializedName("ability2")val ability2 : String?,
+    @SerializedName("ability3")val ability3 : String?,
     @SerializedName("genderType")val genderType : Int,
-    @SerializedName("weight")val weight : Float,
+    @SerializedName("weight")val weight : Double,
     @SerializedName("h")val h: Int,
     @SerializedName("a")val a: Int,
     @SerializedName("b")val b: Int,
