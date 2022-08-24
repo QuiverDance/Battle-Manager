a27abf6a53f02bfaf417b7f6f307d54353787412
여름 <pjw7200@ajou.ac.kr>
2022-08-24 22:34:06 +0900

    pokemon 이미지 바인딩

--- app/src/main/java/com/example/battlemanager/presentation/binding/binding.kt
+++ app/src/main/java/com/example/battlemanager/presentation/binding/binding.kt
@@ -1,10 +1,22 @@
 package com.example.battlemanager.presentation.binding
 
+import android.content.res.AssetManager
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
-import com.example.battlemanager.presentation.global.util.GlideUtil
+import com.example.battlemanager.R
+import java.io.InputStream
 
-@BindingAdapter("app:imageUrl")
-fun bindImageUrl(view: ImageView, imageUrl: String?) {
-    GlideUtil.setImage(imageUrl, view)
+
+@BindingAdapter("app:urlInAssets")
+fun bindUrlInAssets(view: ImageView, imageUrl: String?) {
+    if(imageUrl == null) {
+        view.setImageResource(R.drawable.ic_launcher_foreground)
+        return
+    }
+
+    val assetManager: AssetManager = view.context.resources.assets
+    val inputStream: InputStream = assetManager.open(imageUrl)
+    view.setImageDrawable(Drawable.createFromStream(inputStream, null))
+    inputStream.close()
 }
