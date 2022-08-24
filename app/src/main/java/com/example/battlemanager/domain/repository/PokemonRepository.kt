b3a5c537d4d7096e25c51399563b4b5cff9f3ac2
여름 <pjw7200@ajou.ac.kr>
2022-08-24 20:10:42 +0900

    method 이름 수정

--- app/src/main/java/com/example/battlemanager/domain/repository/PokemonRepository.kt
+++ app/src/main/java/com/example/battlemanager/domain/repository/PokemonRepository.kt
@@ -3,7 +3,7 @@ package com.example.battlemanager.domain.repository
 import com.example.battlemanager.domain.model.PokemonInfo
 
 interface PokemonRepository {
-    fun getPokemonInfoForName(name: String) : PokemonInfo
+    fun getPokemonInfoByName(name: String) : PokemonInfo
     fun getPokemonInfoList() : List<PokemonInfo>
     fun getPokemonNameList() : List<String>
 }
