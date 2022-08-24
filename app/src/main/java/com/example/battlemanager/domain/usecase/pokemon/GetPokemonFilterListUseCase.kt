97b0b4022d7d282fb9bf1aac43d7d80d0a57d883
여름 <pjw7200@ajou.ac.kr>
2022-08-24 20:11:11 +0900

    불필요 코드 삭제

--- app/src/main/java/com/example/battlemanager/domain/usecase/pokemon/GetPokemonFilterListUseCase.kt
+++ app/src/main/java/com/example/battlemanager/domain/usecase/pokemon/GetPokemonFilterListUseCase.kt
@@ -3,30 +3,19 @@ package com.example.battlemanager.domain.usecase.pokemon
 import android.util.Log
 import com.example.battlemanager.data.repository.PokemonRepositoryImpl
 import com.example.battlemanager.domain.model.FilterItem
-import com.example.battlemanager.domain.model.PokemonInfo
-import com.example.battlemanager.domain.repository.PokemonRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class GetPokemonFilterListUseCase @Inject constructor(private val pokemonRepository: PokemonRepositoryImpl) {
-    suspend fun invoke() : List<FilterItem>{
-        val pokemonList = withContext(Dispatchers.IO){
+    suspend fun invoke(): List<FilterItem> {
+        val pokemonList = withContext(Dispatchers.IO) {
             pokemonRepository.getPokemonInfoList()
         }
-        Log.d("포켓몬 리스트 size : ", pokemonList.size.toString())
-        val pokemons = listOf(
-            FilterItem(1, "", "이상해씨"), FilterItem(2, "", "이상해풀"), FilterItem(3, "", "이상해꽃"),
-            FilterItem(4, "", "파이리"), FilterItem(5, "", "리자드"), FilterItem(6, "", "리자몽"),
-            FilterItem(7, "", "꼬부기"), FilterItem(8, "", "어니부기"), FilterItem(9, "", "거북왕")
-        )
-        return pokemons
-//        val filterList = mutableListOf<FilterItem>()
-//        var id = 1
-//        for(pokemon in pokemonList) {
-//            filterList.add(FilterItem(id, "", pokemon))
-//            id += 1
-//        }
-//        return filterList
+        val filterList = mutableListOf<FilterItem>()
+        for (pokemon in pokemonList) {
+            filterList.add(FilterItem(pokemon.id.toInt(), pokemon.imageUrl, pokemon.name))
+        }
+        return filterList
     }
 }
--- app/src/main/java/com/example/battlemanager/domain/usecase/pokemon/GetPokemonInfoUseCase.kt
+++ app/src/main/java/com/example/battlemanager/domain/usecase/pokemon/GetPokemonInfoUseCase.kt
@@ -2,29 +2,16 @@ package com.example.battlemanager.domain.usecase.pokemon
 
 import com.example.battlemanager.data.repository.PokemonRepositoryImpl
 import com.example.battlemanager.domain.model.BaseStats
-import com.example.battlemanager.domain.model.FilterItem
 import com.example.battlemanager.domain.model.PokemonInfo
-import com.example.battlemanager.domain.repository.PokemonRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class GetPokemonInfoUseCase @Inject constructor(private val pokemonRepository: PokemonRepositoryImpl) {
     suspend fun invoke(name : String) : PokemonInfo{
-        val pokemons = listOf(
-            PokemonInfo(1, 1, "이상해씨", "", "풀", "독", listOf("심록"), 3, 5f.toDouble(), BaseStats(45, 49, 49, 65, 65, 45)),
-            PokemonInfo(2, 2, "이상해풀", "", "풀", "독", listOf("심록"), 3, 5f.toDouble(), BaseStats(60, 62, 63, 80, 80, 60)),
-            PokemonInfo(3, 3, "이상해꽃", "", "풀", "독", listOf("심록"), 3, 5f.toDouble(), BaseStats(80, 82, 83, 100, 100, 80)),
-            PokemonInfo(4, 4, "파이리", "", "불", "", listOf("맹화"), 3, 5f.toDouble(), BaseStats(39, 52, 43, 60, 50, 65)),
-            PokemonInfo(5, 5, "리자드", "", "불", "", listOf("맹화"), 3, 5f.toDouble(), BaseStats(58, 64, 58, 80, 65, 80)),
-            PokemonInfo(6, 6, "리자몽", "", "불", "비행", listOf("맹화"), 3, 5f.toDouble(), BaseStats(78, 84, 78, 109, 85, 100)),
-            PokemonInfo(7, 7, "꼬부기", "", "물", "", listOf("급류"), 3, 5f.toDouble(), BaseStats(44, 48, 65, 50, 64, 43)),
-            PokemonInfo(8, 8, "어니부기", "", "물", "", listOf("급류"), 3, 5f.toDouble(), BaseStats(59, 63, 80, 65, 80, 58)),
-            PokemonInfo(9, 9, "거북왕", "", "물", "", listOf("급류"), 3, 5f.toDouble(), BaseStats(79, 83, 100, 85, 105, 78)),
-        )
-        for(pokemon in pokemons){
-            if(name == pokemon.name)
-                return pokemon
+        val pokemon = withContext(Dispatchers.IO){
+            pokemonRepository.getPokemonInfoByName(name)
         }
-        return pokemons[0]
-//        return pokemonRepository.getPokemonInfoForName(name)
+        return pokemon
     }
 }
