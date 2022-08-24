8699ddcd72835f7f0250b662b0cf3cc28d75b033
여름 <pjw7200@ajou.ac.kr>
2022-08-24 20:25:16 +0900

    glide 관련 util 추가

---
+++ app/src/main/java/com/example/battlemanager/global/util/GlideUtil.kt
@@ -0,0 +1,18 @@
+package com.example.battlemanager.global.util
+
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.example.battlemanager.R
+
+object GlideUtil {
+    fun setImage(url: String?, view : ImageView){
+        if(url == null) return
+
+        Glide.with(view.context)
+            .load(url)
+            .placeholder(R.drawable.ic_launcher_foreground)
+            .error(R.drawable.ic_launcher_foreground)
+            .centerCrop()
+            .into(view)
+    }
+}
