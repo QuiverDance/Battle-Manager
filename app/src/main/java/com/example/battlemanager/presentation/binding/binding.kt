decb252a95a0507580b41e5d878681dce7d61afc
QuiverDance <pjw7200@ajou.ac.kr>
2022-08-29 14:52:53 +0900

    타입 배경색 바인딩 추가

--- app/src/main/java/com/example/battlemanager/presentation/binding/binding.kt
+++ app/src/main/java/com/example/battlemanager/presentation/binding/binding.kt
@@ -2,9 +2,11 @@ package com.example.battlemanager.presentation.binding
 
 import android.content.res.AssetManager
 import android.graphics.drawable.Drawable
+import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.example.battlemanager.R
+import com.example.battlemanager.presentation.global.util.TypeUtil
 import java.io.InputStream
 
 
@@ -19,4 +21,12 @@ fun bindUrlInAssets(view: ImageView, imageUrl: String?) {
     val inputStream: InputStream = assetManager.open(imageUrl)
     view.setImageDrawable(Drawable.createFromStream(inputStream, null))
     inputStream.close()
+}
+
+@BindingAdapter("app:typeBackground")
+fun bindTypeBackground(view: View, type: String?){
+    if(type == null)
+        return
+
+    view.setBackgroundColor(TypeUtil.getTypeColor(type))
 }
