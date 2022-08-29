29b543ec2542676900bd22a6226fc45b8bd2dc2e
QuiverDance <pjw7200@ajou.ac.kr>
2022-08-29 17:02:37 +0900

    setBackgroundResource 사용

--- app/src/main/java/com/example/battlemanager/presentation/binding/binding.kt
+++ app/src/main/java/com/example/battlemanager/presentation/binding/binding.kt
@@ -2,11 +2,13 @@ package com.example.battlemanager.presentation.binding
 
 import android.content.res.AssetManager
 import android.graphics.drawable.Drawable
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.example.battlemanager.R
 import com.example.battlemanager.presentation.global.util.TypeUtil
+import java.io.FileNotFoundException
 import java.io.InputStream
 
 
@@ -17,16 +19,20 @@ fun bindUrlInAssets(view: ImageView, imageUrl: String?) {
         return
     }
 
-    val assetManager: AssetManager = view.context.resources.assets
-    val inputStream: InputStream = assetManager.open(imageUrl)
-    view.setImageDrawable(Drawable.createFromStream(inputStream, null))
-    inputStream.close()
+    try {
+        val assetManager: AssetManager = view.context.resources.assets
+        val inputStream: InputStream = assetManager.open(imageUrl)
+        view.setImageDrawable(Drawable.createFromStream(inputStream, null))
+        inputStream.close()
+    }catch (e : FileNotFoundException){
+        view.setImageResource(R.drawable.ic_launcher_foreground)
+        return
+    }
 }
 
 @BindingAdapter("app:typeBackground")
 fun bindTypeBackground(view: View, type: String?){
     if(type == null)
         return
-
-    view.setBackgroundColor(TypeUtil.getTypeColor(type))
+    view.setBackgroundResource(TypeUtil.getTypeBackground(type))
 }
