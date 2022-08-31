2f495a30554455abd1957732371087a2cd6c5e4d
QuiverDance <pjw7200@ajou.ac.kr>
2022-08-31 21:29:48 +0900

    선재 공격 여부 확인 기능 추가

--- app/src/main/java/com/example/battlemanager/presentation/main/MainFragment.kt
+++ app/src/main/java/com/example/battlemanager/presentation/main/MainFragment.kt
@@ -1,5 +1,6 @@
 package com.example.battlemanager.presentation.main
 
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -11,6 +12,7 @@ import com.example.battlemanager.databinding.FragmentMainBinding
 import com.example.battlemanager.presentation.global.base.BaseFragment
 import com.example.battlemanager.presentation.PokemonMemory
 import com.example.battlemanager.presentation.global.util.FieldUtil
+import com.example.battlemanager.presentation.global.util.RealStatUtil
 import com.example.battlemanager.presentation.global.util.StatusAbnormalityUtil
 import com.example.battlemanager.presentation.pokemonSetting.PokemonSettingViewPagerAdapter
 import com.google.android.material.tabs.TabLayout
@@ -28,32 +30,40 @@ class MainFragment : BaseFragment<FragmentMainBinding>() {
     override fun initDataBinding() {
         super.initDataBinding()
         binding.viewModel = viewModel
-        viewModel.startPokemon1.observe(this){
+        viewModel.startPokemon1.observe(this) {
             val bundle = bundleOf("pos" to 1)
             findNavController().navigate(R.id.action_mainFragment_to_pokemonSettingFragment, bundle)
         }
-        viewModel.startPokemon2.observe(this){
+        viewModel.startPokemon2.observe(this) {
             val bundle = bundleOf("pos" to 2)
             findNavController().navigate(R.id.action_mainFragment_to_pokemonSettingFragment, bundle)
         }
-        viewModel.startResult.observe(this){
-            if(PokemonMemory.getIsValid(1) && PokemonMemory.getIsValid(2))
+        viewModel.startResult.observe(this) {
+            if (PokemonMemory.getIsValid(1) && PokemonMemory.getIsValid(2))
                 findNavController().navigate(R.id.action_mainFragment_to_ResultFragment)
         }
-        viewModel.startSwap.observe(this){
+        viewModel.startSwap.observe(this) {
             PokemonMemory.swapPokemon()
             setPokemon()
         }
+        viewModel.weather.observe(this){
+            if(PokemonMemory.getIsValid(1) && PokemonMemory.getIsValid(2))
+                viewModel.setPreemptivePokemon()
+        }
     }
-    private fun setPokemon(){
-        if(PokemonMemory.getIsValid(1)){
+
+    private fun setPokemon() {
+        if (PokemonMemory.getIsValid(1)) {
             viewModel.setPokemon(PokemonMemory.getPokemon(1), 1)
         }
-        if(PokemonMemory.getIsValid(2)){
+        if (PokemonMemory.getIsValid(2)) {
             viewModel.setPokemon(PokemonMemory.getPokemon(2), 2)
         }
+        if(PokemonMemory.getIsValid(1) && PokemonMemory.getIsValid(2))
+            viewModel.setPreemptivePokemon()
     }
-    private fun setSpinner(){
+
+    private fun setSpinner() {
         val weatherList = FieldUtil.getWeatherList()
         val weatherAdapter = ArrayAdapter(
             requireContext(),
@@ -80,15 +90,15 @@ class MainFragment : BaseFragment<FragmentMainBinding>() {
 
             }
 
-        val fieldList = FieldUtil.getWeatherList()
+        val fieldList = FieldUtil.getFieldList()
         val fieldAdapter = ArrayAdapter(
             requireContext(),
             androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
-            weatherList
+            fieldList
         )
         fieldAdapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item)
-        binding.weatherSpinner.adapter = fieldAdapter
-        binding.weatherSpinner.onItemSelectedListener =
+        binding.fieldSpinner.adapter = fieldAdapter
+        binding.fieldSpinner.onItemSelectedListener =
             object : AdapterView.OnItemSelectedListener {
                 override fun onItemSelected(
                     parent: AdapterView<*>?,
--- app/src/main/java/com/example/battlemanager/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/example/battlemanager/presentation/main/MainViewModel.kt
@@ -1,9 +1,12 @@
 package com.example.battlemanager.presentation.main
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.battlemanager.domain.model.Pokemon
+import com.example.battlemanager.presentation.PokemonMemory
+import com.example.battlemanager.presentation.global.util.RealStatUtil
 import com.example.battlemanager.presentation.global.util.SingleLiveEvent
 import com.example.battlemanager.presentation.global.util.StatUtil
 
@@ -31,8 +34,51 @@ class MainViewModel : ViewModel() {
     val weather: LiveData<String> get() = _weather
     val field: LiveData<String> get() = _field
 
-    fun setWeather(weather: String) = _weather.postValue(weather)
-    fun setField(field: String) = _field.postValue(field)
+    fun setWeather(value: String) = _weather.postValue(value)
+    fun setField(value: String) = _field.postValue(value)
+
+    private val _preemptivePokemon = MutableLiveData<Int>(0)
+    val preemptivePokemon: LiveData<Int> get() = _preemptivePokemon
+
+    fun setPreemptivePokemon() {
+        val pokemon1 = PokemonMemory.getPokemon(1)
+        val pokemon2 = PokemonMemory.getPokemon(2)
+
+        val speed1 = RealStatUtil.getSpeed(
+            pokemon1.level,
+            pokemon1.pokemonInfo.baseStats.S,
+            pokemon1.individualValues.S,
+            pokemon1.effortValues.S,
+            pokemon1.nature,
+            pokemon1.rankStates,
+            pokemon1.ability,
+            pokemon1.item,
+            pokemon1.statusAbnormality,
+            weather.value!!
+        )
+        val speed2 = RealStatUtil.getSpeed(
+            pokemon2.level,
+            pokemon2.pokemonInfo.baseStats.S,
+            pokemon2.individualValues.S,
+            pokemon2.effortValues.S,
+            pokemon2.nature,
+            pokemon2.rankStates,
+            pokemon2.ability,
+            pokemon2.item,
+            pokemon2.statusAbnormality,
+            weather.value!!
+        )
+        Log.d("pokemon1", pokemon1.toString())
+        Log.d("pokemon2", pokemon2.toString())
+        Log.d("speed", "$speed1 ? $speed2")
+        Log.d("weather", _weather.value!!)
+        if(speed1 > speed2)
+            _preemptivePokemon.postValue(1)
+        else if(speed2 > speed1)
+            _preemptivePokemon.postValue(2)
+        else
+            _preemptivePokemon.postValue(0)
+    }
 
     val startPokemon1 = SingleLiveEvent<Any>()
     fun startPokemon1() {
