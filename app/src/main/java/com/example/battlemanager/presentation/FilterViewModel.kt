5cff2d6f69f8eb04fd568e8e6f85e73ee20290d4
QuiverDance <pjw7200@ajou.ac.kr>
2022-09-02 22:59:18 +0900

    FilterViewModel 추가

---
+++ app/src/main/java/com/example/battlemanager/presentation/FilterViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.battlemanager.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.battlemanager.domain.model.CategoryItem
+import com.example.battlemanager.domain.model.FilterItem
+import com.example.battlemanager.presentation.global.util.SingleLiveEvent
+
+class FilterViewModel : ViewModel() {
+    private val _selectedCategory = MutableLiveData<Int>(0)
+    val selectedCategory: LiveData<Int> get() = _selectedCategory
+    fun selectCategory(pos: Int){
+        _selectedCategory.postValue(pos)
+    }
+
+    fun getCategoryList(list: List<String>) : List<CategoryItem>{
+        val categoryList = mutableListOf<CategoryItem>()
+        for(value in list){
+            categoryList.add(CategoryItem(value, false))
+        }
+        return categoryList
+    }
+
+    fun getFilterItemByCategory(list: List<FilterItem>, category: String): List<FilterItem>{
+        val filterItemList = mutableListOf<FilterItem>()
+        for(item in list){
+            if(item.Type == category)
+                filterItemList.add(item)
+        }
+        return filterItemList
+    }
+
+    val startResult = SingleLiveEvent<Any>()
+    fun onStartResult() {
+        startResult.call()
+    }
+
+    val startSwap = SingleLiveEvent<Any>()
+    fun onStartSwap() {
+        startSwap.call()
+    }
+}
