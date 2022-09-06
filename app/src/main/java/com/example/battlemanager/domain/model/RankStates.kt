1f90ba6b8f14369c6ae13fb50a496de12139fa9f
QuiverDance <pjw7200@ajou.ac.kr>
2022-09-06 20:18:52 +0900

    변수 이름 수정

--- app/src/main/java/com/example/battlemanager/domain/model/RankStates.kt
+++ app/src/main/java/com/example/battlemanager/domain/model/RankStates.kt
@@ -4,13 +4,13 @@ data class RankStates(
     val attack : Int,
     val defense : Int,
     val specialAttack : Int,
-    val special_defense : Int,
+    val specialDefense : Int,
     val speed : Int,
     val accuracy : Int,
     val evasionRate : Int,
     val criticalRate : Int
 ){
     override fun toString(): String {
-        return "A : $attack B: $defense C: $specialAttack D: $special_defense S: $speed ACC: $speed EVA: $evasionRate CRI: $criticalRate"
+        return "A : $attack B: $defense C: $specialAttack D: $specialDefense S: $speed ACC: $speed EVA: $evasionRate CRI: $criticalRate"
     }
 }
